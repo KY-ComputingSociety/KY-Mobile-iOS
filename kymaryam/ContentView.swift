ce1609e2f90881526f842b55ac660239b1157a32
Maryam <kycomputingsociety@gmail.com>
2021-01-12 09:07:12 +0800

    Initial Commit

---
+++ kymaryam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kymaryam
+//
+//  Created by Maryam Safiyah Widad Mohd Azmi on 12/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kymaryam/kymaryamApp.swift
@@ -0,0 +1,17 @@
+//
+//  kymaryamApp.swift
+//  kymaryam
+//
+//  Created by Maryam Safiyah Widad Mohd Azmi on 12/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct kymaryamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
